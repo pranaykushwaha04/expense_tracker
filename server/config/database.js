82501662ee3bdd9162a1e5f5249bc9b740fde8b8
pranaykushwaha04 <pranay.kushwaha9@gmail.com>
2025-11-12 15:03:00 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'expense_tracker',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME || 'ExpenseDB',
+  process.env.DB_USER || 'admin',
+  process.env.DB_PASSWORD || 'Chunmunsnt@512',
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST || 'expensedb.c56kwqkvtgel.us-east-1.rds.amazonaws.com',
+    port: process.env.DB_PORT || 3307,
     dialect: 'mysql',
     logging: false,
     pool: {
